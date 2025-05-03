4ac2bbddff33e70c382048dcf2c275404066a342
andres27461 <andres2746@gmail.com>
2025-05-03 11:59:03 -0500

    work in class 4

--- homework/tests/test_migracion.py
+++ homework/tests/test_migracion.py
@@ -8,7 +8,7 @@ def test_migracion():
 
     try:
         subprocess.run(
-            ["python", "-m", "homework", "data/input", "data/output"],
+            ["python3", "-m", "homework", "data/input", "data/output"],
             check=True,
         )
     except subprocess.CalledProcessError as e:
